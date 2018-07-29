ce0609c7396e6276c5f57282bdddf3dbc63e0d0c
Navneet Gupta <gupta-navneet@live.com>
2018-07-28 23:22:06 -0700

    Don't reload the data after opening the form

--- src/FeatureTimeline/redux/sagas/launchWorkItemFormSaga.ts
+++ src/FeatureTimeline/redux/sagas/launchWorkItemFormSaga.ts
@@ -1,9 +1,9 @@
-import { call, put } from "redux-saga/effects";
+import { call } from "redux-saga/effects";
 import { IWorkItemFormNavigationService, WorkItemFormNavigationService } from "TFS/WorkItemTracking/Services";
 import { LaunchWorkItemFormAction } from "../../../Common/redux/actions/launchWorkItemForm";
-import { getProjectId, getTeamId } from "../../../Common/redux/Selectors/CommonSelectors";
-import { getBacklogLevel } from "../selectors";
-import { createInitialize, resetAllData } from "../store/common/actioncreators";
+// import { getProjectId, getTeamId } from "../../../Common/redux/Selectors/CommonSelectors";
+// import { getBacklogLevel } from "../selectors";
+//import { createInitialize, resetAllData } from "../store/common/actioncreators";
 
 
 export function* launchWorkItemFormSaga(action: LaunchWorkItemFormAction) {
@@ -13,10 +13,10 @@ export function* launchWorkItemFormSaga(action: LaunchWorkItemFormAction) {
     // TODO: At this point the workitem returned after the update does not have
     // updated links so we do not have a way to identify if any of the links changed
     // our best bet is to update the workitems and relations by reinitializing
-    const projectId = getProjectId();
-    const teamId = getTeamId();
-    const backlogLevelName = getBacklogLevel();
+    // const projectId = getProjectId();
+    // const teamId = getTeamId();
+    // const backlogLevelName = getBacklogLevel();
 
-    yield put(resetAllData());
-    yield put(createInitialize(projectId, teamId, backlogLevelName));
+    // yield put(resetAllData());
+    // yield put(createInitialize(projectId, teamId, backlogLevelName));
 }
