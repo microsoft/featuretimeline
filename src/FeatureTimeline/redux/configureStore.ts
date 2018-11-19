919eb3b4c8c18c91bfd3b9aea41e77633280b60e
Navneet Gupta <gupta-navneet@live.com>
2018-11-18 18:27:27 -0800

    Initial persisted store

--- src/FeatureTimeline/redux/configureStore.ts
+++ src/FeatureTimeline/redux/configureStore.ts
@@ -3,6 +3,8 @@ import { reducers, IFeatureTimelineRawState } from './store/types';
 import createSagaMiddleware from 'redux-saga'
 import { trackActions } from './sagas/trackActions';
 import { watchSagaActions } from './sagas';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 
 export default function configureFeatureTimelineStore(
     initialState: IFeatureTimelineRawState
@@ -15,9 +17,16 @@ export default function configureFeatureTimelineStore(
     // Setup for using the redux dev tools in chrome 
     // https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
     const composeEnhancers = window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] || compose;
-    
+    const persistConfig = {
+        key: 'root',
+        storage,
+      }
+      
+    const persistedReducer = persistReducer(persistConfig, reducers)
+
+
     const store = createStore(
-        reducers,
+        persistedReducer,
         initialState, 
         composeEnhancers(middleware));
 
